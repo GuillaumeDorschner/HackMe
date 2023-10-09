5c6061ad2c94f7905faaceee3adcd1c978e997e1
Guillaume Dorschner <Guillaume.dorschner@gmail.com>
2023-10-09 16:31:38 +0200

    ip for the post / get / script database

--- client/src/routes/write/+page.svelte
+++ client/src/routes/write/+page.svelte
@@ -1,4 +1,10 @@
 <script>
   import { onMount } from "svelte";
   import "../../app.css";
+
+  let backendUrl;
+
+  onMount(() => {
+    backendUrl = `http://${window.location.hostname}:3001/`;
+  });
 </script>
